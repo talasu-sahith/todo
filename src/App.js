babed1c0bf2b001b987c9abca79e828bf889d4ba
talasu-sahith <sahithtalasu.123@gmail.com>
2024-01-26 20:17:36 +0530

    chore: chakra-ui Integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box, Input, Text } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Flex, Spacer } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import List from "./components/List";
+import { useState } from "react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [inp, setInp] = useState("");
+
+  const [varArr, setVarArr] = useState([]);
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setVarArr([...varArr, { task: inp, id: nanoid() }]);
+    setInp("");
+  };
+  const handledel = (id) => {
+    setVarArr(varArr.filter((k) => k.id !== id));
+    console.log(varArr);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React</h1>
+      <Container>
+        <Heading textAlign="center">Todo</Heading>
+        <form onSubmit={handleAdd}>
+          <Flex gap="6" align="center">
+            <Box>
+              <Input
+                value={inp}
+                variant="flushed"
+                placeholder="Enter task"
+                onChange={(e) => {
+                  setInp(e.target.value);
+                }}
+              />
+            </Box>
+            <Box>
+              <Button
+                colorScheme="teal"
+                variant="link"
+                paddingTop="2"
+                fontSize="22"
+                type="submit"
+              >
+                Add
+              </Button>
+            </Box>
+          </Flex>
+        </form>
+
+        {varArr.map((parArr, index) => (
+          <Card key={index} margin="4" border="solid">
+            <Flex>
+              <CardBody>
+                <Text>{parArr.task}</Text>
+              </CardBody>
+
+              <Button
+                colorScheme="red"
+                margin="2"
+                fontSize="22"
+                onClick={() => handledel(parArr.id)}
+              >
+                Delete
+              </Button>
+            </Flex>
+          </Card>
+        ))}
+      </Container>
+      <List />
     </div>
   );
 }
